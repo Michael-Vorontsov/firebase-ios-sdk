e602d2c6bcec248903b3210911cca16c657f2044
Michael <mykhailo.vorontsov@infopulse.com.ua>
2025-07-09 12:17:10 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
     name: "FirebaseCrashlyticsOnly",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v17),
     ],
     products: [
         .library(
